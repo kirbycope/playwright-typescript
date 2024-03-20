192c33a105bd977ab5ee74b11229fa857b569bcf
Timothy Cope <kirbycope@gmail.com>
2024-03-19 21:19:49 -0700

    Update login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,12 @@
+// Feature: The Internet Guinea Pig Website
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login.page';
 
+// Scenario Outline: As a user, I can log into the secure area
 const scenarios = [
   { username: 'valid', password: 'valid', message: 'You logged into a secure area!' },
   { username: 'foobar', password: 'barfoo', message: 'Your username is invalid!' },
 ];
-
 scenarios.forEach(({ username, password, message }) => {
   test(`login as ${username}`, async ({ page }) => {
 
