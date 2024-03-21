16d8a8f2a06ff49e907920d5a5470ef023687288
Timothy Cope <kirbycope@gmail.com>
2024-03-20 18:09:18 -0700

    working test

--- tests/fixtures/login.fixture.ts
+++ tests/fixtures/login.fixture.ts
@@ -3,7 +3,7 @@ import LoginPage from "../pages/login.page";
 
 /** Extend the base page with the login page object. */
 export const test = base.extend<{loginPage: LoginPage;}>({
-  loginPage: async ({ page }, use) => {
+    loginPage: async ({ page }, use) => {
     await use(new LoginPage(page));
   }
 });
